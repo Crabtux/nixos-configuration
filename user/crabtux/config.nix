13bf7731dffbf636a9910f0d1eaa6330bb02c9ca
Crabtux <crabtux@mail.ustc.edu.cn>
2023-09-28 17:07:36 +0800

    Set fish as the default shell

--- user/crabtux/config.nix
+++ user/crabtux/config.nix
@@ -4,9 +4,10 @@
   # If I don't repeat it here, it will generate errors.
   programs.fish.enable = true;
 
+  users.defaultUserShell = pkgs.fish;
+
   users.users.crabtux = {
     isNormalUser = true;
     extraGroups = [ "networkmanager" "wheel" "audio" "adbusers" ]; 
-    shell = pkgs.fish;
   };
 }

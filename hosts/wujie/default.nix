92b9b48362000a8025cbe66988027a72496d3e27
Crabtux <crabtux@mail.ustc.edu.cn>
2024-01-22 16:57:18 +0800

    Remove unnecessary file: wujie-config.nix

--- hosts/wujie/default.nix
+++ hosts/wujie/default.nix
@@ -1,13 +1,40 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [ 
-    # Template
     ../../profiles/nixos/desktop.nix
-
-    # Local configs
     ./hardware-configuration.nix
-    ./wujie-config.nix
+  ];
+
+  # 机器特定配置
+  networking.hostName = "wujie";
+
+  # Use the systemd-boot EFI boot loader.
+  # TODO: Maybe use grub2?
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Configure the NVIDIA graphic card.
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  hardware.opengl.enable = true;
+  hardware.nvidia.modesetting.enable = true;
+  
+  services.xserver.videoDrivers = [ "modesetting" "nvidia" ];
+
+  boot.supportedFilesystems = [ "ntfs" ];
+
+  boot.kernelPackages = pkgs.linuxPackages_6_1;
+
+  # Use the kernel patch to make i915 work correctly.
+  boot.kernelPatches = [{
+    name = "patch-i915";
+    patch = ./kernel_patch/i915.patch;
+  }];
+
+  # Force to use the ifname '0' to solve a problem.
+  boot.kernelParams = [
+    "net.ifnames=0"
+    "biosdevname=0"
   ];
 
   # This value determines the NixOS release from which the default

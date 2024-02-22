8897ca6e18c86dcd6680105e54840ab3199869bd
Crabtux <crabtux@mail.ustc.edu.cn>
2024-02-22 16:19:06 +0800

    flake: Add NUR input

--- flake.nix
+++ flake.nix
@@ -4,13 +4,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nur.url = "github:nix-community/NUR";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: 
+  outputs = { self, nixpkgs, home-manager, nur, ... }@inputs: 
     let
       inherit (self) outputs;
       system = "x86_64-linux";
@@ -26,6 +27,7 @@
           modules = 
             [
               ./hosts/wujie
+              nur.nixosModules.nur
               home-manager.nixosModules.home-manager
               {
                 home-manager.useGlobalPkgs = true;

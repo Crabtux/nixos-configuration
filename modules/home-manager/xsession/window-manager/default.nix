db0ed373977f97b5d359223ba266fedac9d28b4a
Crabtux <crabtux@mail.ustc.edu.cn>
2024-07-25 19:29:54 +0800

    modules/home-manager/xsession: refactor window-manager directory

--- modules/home-manager/xsession/window-manager/default.nix
+++ modules/home-manager/xsession/window-manager/default.nix
@@ -1,7 +1,24 @@
-{ config, lib, vars, ... }:
+{ config, lib, pkgs, ... }:
 
-{
+let 
+  vars = {
+    # A simple derivation as a wrapper for polybar config
+    polybar-config = pkgs.stdenv.mkDerivation {
+      name = "polybar-config";
+      phases = [ "unpackPhase" "installPhase" ];
+      src = ./bar;
+      installPhase = ''
+        mkdir -p $out
+        cp -r $src/docky/ $out
+      '';
+    };
+  };
+in {
   imports = [
+    ( import ./bar { inherit pkgs vars; } )
     ( import ./i3wm.nix { inherit config lib vars; } )
+    ./app-menu
+    ./compositor
+    ./screensaver
   ];
 }

ef9f2237071921cb1dc5e5de45efd36d670d0988
Crabtux <crabtux@mail.ustc.edu.cn>
2024-07-28 19:46:40 +0800

    modules/home-manager/software/wezterm.nix: fix wrong color for built-in rose-pine colorscheme

--- modules/home-manager/software/wezterm.nix
+++ modules/home-manager/software/wezterm.nix
@@ -15,7 +15,49 @@
         config = wezterm.config_builder()
       end
 
-      config.color_scheme = 'rose-pine'
+      -- The wezterm's built-in internal rose-pine color_scheme uses wrong
+      -- color, so we will override it here.
+      -- ref: https://github.com/neapsix/wezterm/blob/main/plugin/init.lua
+      local main_palette = {
+        base = '#191724',
+        overlay = '#26233a',
+        muted = '#6e6a86',
+        text = '#e0def4',
+        love = '#eb6f92',
+        gold = '#f6c177',
+        rose = '#ebbcba',
+        pine = '#31748f',
+        foam = '#9ccfd8',
+        iris = '#c4a7e7',
+        highlight_high = '#524f67',
+      }
+
+      local rose_pine = wezterm.color.get_builtin_schemes()['rose-pine']
+      rose_pine.ansi = {
+          main_palette.overlay,
+          main_palette.love,
+          main_palette.pine,
+          main_palette.gold,
+          main_palette.foam,
+          main_palette.iris,
+          main_palette.rose,
+          main_palette.text,
+      }
+      rose_pine.brights = {
+          main_palette.muted,
+          main_palette.love,
+          main_palette.pine,
+          main_palette.gold,
+          main_palette.foam,
+          main_palette.iris,
+          main_palette.rose,
+          main_palette.text,
+      }
+      config.color_schemes = {
+        ['Patched rose-pine'] = rose_pine
+      }
+      config.color_scheme = 'Patched rose-pine'
+
       config.enable_scroll_bar = true
       config.use_fancy_tab_bar = false
       config.tab_max_width = 32

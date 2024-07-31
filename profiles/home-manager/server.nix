6d0a75c9ec1feeb0f0ba16e5070ca0263a4bb7e6
Crabtux <crabtux@mail.ustc.edu.cn>
2024-07-31 20:22:35 +0800

    profiles/home-manager/server.nix: use rain to config machine

--- profiles/home-manager/server.nix
+++ profiles/home-manager/server.nix
@@ -5,28 +5,21 @@
     outputs.homeManagerModules.rain
   ];
 
-  targets.genericLinux.enable = true;
-
   home = {
     username = "crabtux";
     homeDirectory = "/home/crabtux";
   };
 
-  home.packages = with pkgs; [
-    nil
-    tmux
-    neofetch
-    clash-meta
-  ];
-
-  # Enable home-manager
-  programs.home-manager.enable = true;
-  programs.bash = {
-    enable = true;
+  rain.home = {
+    software = {
+      cli.enable = true;
+    };
   };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
+
+  targets.genericLinux.enable = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.11";

9e9a884737cc2a3517e1d758ca891bac1a3e4c88
Crabtux <crabtux@mail.ustc.edu.cn>
2025-08-07 16:23:26 +0800

    hosts/wujie: Add wine
    也是熵密杯临时用的，记得整理

--- hosts/wujie/default.nix
+++ hosts/wujie/default.nix
@@ -1,4 +1,4 @@
-{ config, outputs, ... }:
+{ config, pkgs, outputs, ... }:
 
 {
   imports = [ 
@@ -63,6 +63,28 @@
         Option "PreferredMode" "2560x1440"
       '';
     }
+  ];
+
+  environment.systemPackages = with pkgs; [
+    # ...
+
+    # support both 32-bit and 64-bit applications
+    wineWowPackages.stable
+
+    # support 32-bit only
+    wine
+
+    # support 64-bit only
+    (wine.override { wineBuild = "wine64"; })
+
+    # support 64-bit only
+    wine64
+
+    # wine-staging (version with experimental features)
+    wineWowPackages.staging
+
+    # winetricks (all versions)
+    winetricks
   ];
 
 

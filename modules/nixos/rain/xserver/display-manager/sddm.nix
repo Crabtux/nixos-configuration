29d038494aedd8f6ecde32156903f8ac6a66f575
Crabtux <crabtux@mail.ustc.edu.cn>
2025-12-16 23:28:12 +0800

    modules/rain/sddm: Use astronuat theme

--- modules/nixos/rain/xserver/display-manager/sddm.nix
+++ modules/nixos/rain/xserver/display-manager/sddm.nix
@@ -1,16 +1,25 @@
-{ config, lib, outputs, ... }:
+{ config, pkgs, lib, outputs, ... }:
 
 with lib; {
   config = mkIf (config.rain.xserver.display-manager == "sddm") {
     environment.systemPackages = [
-      outputs.packages.sddm-rose-pine
+      # Deprecated
+      # outputs.packages.sddm-rose-pine
+      (pkgs.sddm-astronaut.override {
+        embeddedTheme = "purple_leaves";
+      })
     ];
 
     services.xserver.displayManager = {
       defaultSession = "xfce+i3";
       sddm = {
         enable = true;
-        theme = "rose-pine";
+        theme = "sddm-astronaut-theme";
+        extraPackages = with pkgs.kdePackages; [
+          qtmultimedia
+          qtsvg
+          qtvirtualkeyboard
+        ];
       };
     };
   };
